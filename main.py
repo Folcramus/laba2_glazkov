9561999b0d454f2c04a5b86ad89df649382611c4
Folcramus <ponopaytov@Gmail.com>
2025-03-02 18:56:53 +0300

    Second commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def grad_phi(x, y):
     return np.array([df_dx, df_dy])
 # Метод скорейшего спуска
 def steepest_descent(x0, y0, tol=1e-6, max_iter=1000):
-    x, y = x0, y0
+    x, y = x0, y0 #начальное приближение
     for i in range(max_iter):
         x_old, y_old = x, y
         grad = grad_phi(x, y)
@@ -22,7 +22,7 @@ def steepest_descent(x0, y0, tol=1e-6, max_iter=1000):
         x = x - lambda_k * grad[0]
         y = y - lambda_k * grad[1]
 
-        # Проверяем условие останова по относительной разнице
+        # Проверяем условие останова по относительной разнице (вычисляем градиент)
         delta_num = abs(x - x_old) + abs(y - y_old)  # это L1-норма разности
         delta_den = abs(x_old) + abs(y_old)  # это L1-норма старой точки
         if delta_den == 0:  # чтобы избежать деления на 0
